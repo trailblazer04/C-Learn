be13b30af7036000b5951076f58b55b35a9868bc
trailblazer04 <leofrancis6988@gmail.com>
2025-11-03 00:30:18 +0530

    tried to use comments and variable

---
+++ Basics/variable.c
@@ -0,0 +1,14 @@
+// This is a comment
+
+// This is a variable Declaration and initialization in c programming language using int data type which is a whole number or interger data type 
+int myNum = 5;
+
+
+// This is a variable declaration in c programming language without initialization 
+int myNum;
+
+// This is a variable initialization in c programming language without declaration 
+myNum = 15;
+
+// This is a variable printing in c programming language using printf function but it will not print the value of the variable because we have not used the %d format specifier to print the value of the variable 
+printf(myNum);
